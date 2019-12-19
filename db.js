961cd2837a650f37d05885c0965b5b1b04321d6d
kuma0416 <49488231+kuma0416@users.noreply.github.com>
2019-12-19 15:58:09 +0800

    Add files via upload

--- db.js
+++ db.js
@@ -1,61 +1,76 @@
 var fs = require('fs');
-var FILENAME = 'data.json';
-var userlist = 'user.json';
+var user = 'user.json';
+var team = 'team.json';
+var guardian = 'guardian.json';
 
-var DB = {
-  addPost: function (post, cb) {
-    DB.getPosts(function (err, posts) {
-      if (err) {
-        return cb(err);
-      }
-      posts.push(post);
-      DB.savePosts(posts, cb);
-    })
-  },
+var db = {
+    loginCheck: function(callback){
+        fs.readFile(user, 'utf-8', function(err, userlist){
+            if (err){
+                callback(err);
+            } else {
+                callback(JSON.parse(userlist));
+            }
+        });
+    },
 
-  deletePost: function (id, cb) {
-    DB.getPosts(function (err, posts) {
-      if (err) {
-        return cb(err);
-      }
-      var index = -1;
-      for(var i = 0; i < posts.length; i++) {
-        if(posts[i].id == id) {
-          index = i;
-          break;
-        }
-      }
-      if (index >=0 ){
-        posts.splice(index, 1);
-      }
-      DB.savePosts(posts, cb);
-    })
-  },
+    getJson: function(filename, callback){
+        var file = filename + ".json";
+        fs.readFile(file, 'utf-8', function(err, data){
+            if(err){
+                callback(err);
+            } else {
+                callback(JSON.parse(data));
+            }
+        });
+    },
 
-  savePosts: function (data, cb) {
-    fs.writeFile(FILENAME, JSON.stringify(data), cb);
-  },
+    save: function(data, filename, callback){
+        var file = filename + ".json"
+        fs.writeFile(file, JSON.stringify(data), callback);
+    },
 
-  getPosts: function (cb) {
-    fs.readFile(FILENAME, 'utf8', function (err, data) {
-      if (err) {
-        cb(err);
-      } else {
-        console.log(data);
-        cb(err, JSON.parse(data));
-      }
-    })
-  },
+    editGuardian: function(username, G1data, G2data, callback){
+        db.getJson("guardian", function(Glist){
+            var nowHP = 0;
+            for(var i = 0;i < Glist.length; i++){
+                if(Glist[i].name == G1data.name){
+                    nowHP = Glist[i].HP - G1data.HP;
+                    if(nowHP <= 0){
+                        Glist[i].count ++;
+                        Glist[i].HP = 20 + Glist[i].count*5;
+                    } else {
+                        Glist[i].HP = nowHP;
+                    }
+                } else if (Glist[i].name == G2data.name){
+                    nowHP = Glist[i].HP - G1data.HP;
+                    if(nowHP <= 0){
+                        Glist[i].count ++;
+                        Glist[i].HP = 20 + Glist[i].count*5;
+                    } else {
+                        Glist[i].HP = nowHP;
+                    }
+                }
+            }
+            db.save(Glist, "guardian", callback);
+        });
+    },
 
-  loginCheck: function(cb) {
-    fs.readFile(userlist, 'utf8', function(err, lis){
-      if (err) {
-        cb(err);
-      } else {
-        cb(err, JSON.parse(lis));
-      }
-    })
-  }
+    editTeam: function(T1data, T2data, callback){
+        db.getJson("team", function(Tlist){
+            var nowScore = 0;
+            for(var i = 0;i < Tlist.length; i++){
+                if(Tlist[i].number == T1data.number){
+                    nowScore = Tlist[i].score + parseInt(T1data.score, 10);
+                    Tlist[i].score = nowScore;
+                } else if (Tlist[i].name == T2data.name){
+                    nowScore = Tlist[i].score + parseInt(T2data.score, 10);
+                    Tlist[i].score = nowScore;
+                }
+            }
+            db.save(Tlist, "team", callback);
+        })
+    }
 }
 
-module.exports = DB;
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,214 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+const port = process.env.PORT || 3000;
+var session = require('express-session');
+var fs = require('fs');
+var db = require('./db');
+
+function render(filename, params) {
+    var data = fs.readFileSync(filename, 'utf8');
+    for (var key in params) {
+      data = data.replace('{' + key + '}', params[key]);
+    }
+  return data;
+}
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+  
+app.set('view engine', 'ejs');
+  
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  account: ""
+}));
+
+app.get('/', function(req, res){
+  db.getJson("guardian", function(guardian){
+    db.getJson("team", function(team){
+      res.render('index', {
+        all: guardian,
+        team: team
+      })
+    })
+  })
+})
+
+app.get('/back', function(req, res){
+  var account = req.session.account;
+  db.getJson("guardian", function(guardian){
+    db.getJson("team", function(team){
+      res.render('back', {
+        all: guardian,
+        team: team,
+        account:account
+      })
+    })
+  })
+})
+
+app.get('/login', function(req, res){
+  res.render('login', {
+    loginSuccess:"notYet"
+  });
+})
+
+app.post('/login', function(req, res){
+  var account = req.body.account;
+  var password = req.body.password;
+  db.loginCheck(function(user){
+    for(var i=0;i<user.length;i++){
+      if(account === user[i].account && password === user[i].password){
+        req.session.account = account;
+        db.getJson("guardian", function(guardian){
+          db.getJson("team", function(team){
+            res.render('back', {
+              all: guardian,
+              team: team,
+              account: account
+            })
+          })
+        })
+      }
+    }
+    if(req.session.account == null){
+      res.render('login', {
+        loginSuccess: "fail"
+      })
+    }
+  })
+})
+
+app.get('/logout', function(req, res){
+  req.session.destroy();
+  res.redirect('/back');
+});
+
+app.get('/back/edit', function(req, res){
+  res.render('edit');
+})
+
+app.post('/edit', function(req, res){
+  var G1data = {name:req.body.guardian1, HP:req.body.HP1};
+  var G2data = {name:req.body.guardian2, HP:req.body.HP2};
+  var T1data = {number:req.body.teamnum1,score:req.body.score1};
+  var T2data = {number:req.body.teamnum2,score:req.body.score2};
+  db.editGuardian("test", G1data, G2data, function(result){});
+  db.editTeam(T1data, T2data, function(result){});
+  res.redirect('/back');
+})
+
+app.listen(port, function(){
+  console.log("server running!");
+});
+/*  app.get('/', function(req, res){
+      var account = req.session.account;
+      var loginState = req.session.loginState;
+      db.getBlood(function(all){
+        db.getTeam(function(team){
+          res.render('index', {
+            all: all,
+            account:"",
+            wrong:"",
+            team: team
+          });
+        });
+      });
+  });
+
+  app.get('/login', function(req, res){
+    res.render('login');
+  });
+
+  app.post('/login', function(req, res){
+    var account = req.body.account;
+    var password = req.body.password;
+    db.loginCheck(account, password, function(login){
+        if(login == "success"){
+          req.session.account = account;
+          req.session.loginState = "true";
+          res.render('back', {
+            account: req.session.account,
+            state: ""
+          })
+        } else {
+          db.getBlood(function(all){
+            db.getTeam(function(team){
+              res.render('index', {
+                all: all,
+                account:"",
+                wrong:"",
+                team: team
+              });
+            });
+          });
+        }
+    })
+  });
+
+  app.get('/update/:account', function(req, res){
+    if(req.session.account){
+      var account = req.session.account;
+      res.render('index_plus', {
+        account: account
+      });
+    } else {
+      db.getBlood(function(all){
+        db.getTeam(function(team){
+          res.render('index', {
+            all: all,
+            account:"",
+            wrong:"wrongPath",
+            team: team
+          });
+        });
+      });
+    }
+  });
+
+  app.post('/update/:account', function(req, res){
+    var blood = req.body.blood;
+    var account = req.session.account;
+    db.editBlood(account, blood, function(state){
+        console.log(state);
+        db.getBlood(function(all){
+          res.render('back', {
+            account:account,
+            state: state
+          })
+        })
+    });
+  });
+
+  app.get('/nowblood', function(req, res){
+    db.getBlood(function(all){
+      db.getTeam(function(team){
+        res.render('index', {
+          all: all,
+          account:"",
+          wrong:"",
+          team: team
+        });
+      });
+    });
+  })
+
+  app.get('/team', function(req, res){
+    res.render('team');
+  })
+
+  app.post('/team', function(req, res){
+    var plusScore = req.body.score;
+    var teamNo = req.body.teamnum;
+    db.editScore(teamNo, plusScore, function(state){
+      res.render('back', {
+        account: req.session.account,
+        state: state
+      })
+    })
+  })*/
